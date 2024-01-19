6b547c02cae458347048720e383cbbc84e49d011
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-01-19 14:29:42 +0200

    Add files via upload

---
+++ 19.01.2024/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object[] arr = new object[6];
+            //arr[0] = 1859;
+            //arr[1] = -7;
+            //arr[2] = 'g';
+            //arr[3] = "hello";
+            //arr[4] = null;
+            //arr[5] = new object();
+            //foreach (var elem in arr)
+            //{
+            //    Console.WriteLine(elem);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+           Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 19.01.2024/ConsoleApp1/School.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class School
+    {
+        /// <summary>
+        /// поля ФИО
+        /// </summary>
+        private string name;
+
+        private string surname;
+
+        private string patronymic;
+
+        /// <summary>
+        /// поле пола
+        /// </summary>
+        private string gender;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Surname { get { return surname; } set { surname = value; } }
+        public string Patronymic { get { return patronymic; } set { patronymic = value; } }
+
+        public string Gender { get { return gender; } set { gender = value; } }
+
+        /// <summary>
+        /// конструктор с параметрами
+        /// </summary>
+        public School(string name, string surname, string patronymic, string gender)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Patronymic = patronymic;
+            this.Gender = gender;
+        }
+
+        
+
+
+
+
+
+
+
+
+    }
+}
